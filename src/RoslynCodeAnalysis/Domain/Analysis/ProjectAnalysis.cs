f4deca97377a37fb76c5ac9a437e3008285f7fc6
gBritz <gbritzv@gmail.com>
2015-05-23 03:28:47 -0300

    feat: Create principal domain

---
+++ src/RoslynCodeAnalysis/Domain/Analysis/ProjectAnalysis.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RoslynCodeAnalysis.Domain.Analysis
+{
+    public class ProjectAnalysis
+    {
+        public String AssemblyName { get; set; }
+
+        public String DefaultNamespace { get; set; }
+
+        public Int32 ClassesCount
+        {
+            get { return Files.Sum(f => f.ClassesCount); }
+        }
+
+        public Int32 MethodsCount
+        {
+            get { return Files.Sum(f => f.MethodsCount); }
+        }
+
+        public Int32 FieldsCount
+        {
+            get { return Files.Sum(f => f.FieldsCount); }
+        }
+
+        public Int32 PropertiesCount
+        {
+            get { return Files.Sum(f => f.PropertiesCount); }
+        }
+
+        public Int32 FilesCount
+        {
+            get { return Files.Count(); }
+        }
+
+        public Int32 LogicalCodeLinesCount
+        {
+            get { return Files.Sum(f => f.LogicalCodeLinesCount); }
+        }
+
+        public ICollection<FileAnalysis> Files { get; set; }
+    }
+}
---
+++ src/RoslynCodeAnalysis/Domain/Analysis/SolutionAnalysis.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace RoslynCodeAnalysis.Domain.Analysis
+{
+    [DebuggerDisplay("{Name} TotalLines={LogicalCodeLinesCount}")]
+    public class SolutionAnalysis
+    {
+        public SolutionAnalysis()
+        {
+            Projects = new List<ProjectAnalysis>();
+        }
+
+        public String Name { get; set; }
+
+        public Int32 LogicalCodeLinesCount
+        {
+            get { return Projects.Sum(p => p.LogicalCodeLinesCount); }
+        }
+
+        public ICollection<ProjectAnalysis> Projects { get; private set; }
+    }
+}
