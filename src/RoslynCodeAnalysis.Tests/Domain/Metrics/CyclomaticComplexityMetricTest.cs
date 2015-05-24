915afc1c71cef70a391bb58c351afbe009987afa
gBritz <gbritzv@gmail.com>
2015-05-24 02:24:48 -0300

    test: More 12 tests

--- src/RoslynCodeAnalysis.Tests/Domain/Metrics/CyclomaticComplexityMetricTest.cs
+++ src/RoslynCodeAnalysis.Tests/Domain/Metrics/CyclomaticComplexityMetricTest.cs
@@ -212,9 +212,259 @@ public int NullCoalescingOperatorMethod(object i)
 
         #endregion
 
-        #region Two statements
+        #region Two consecutive statements
 
         //Duas chamadas consecutivas de: if, while, for, foreach, case, default, continue, goto, &&, ||, catch, operador ternário (?:, ??)
+
+        [TestMethod]
+        public void Given_method_with_2_ifs_of_1_condition_when_metric_measure_should_be_3()
+        {
+            var code = @"
+public void CalculoMaluco(int i)
+{
+    var res = 5;
+    if (i > 10)
+        i += 15;
+    if (i > 20)
+        i += 5;
+    return res;
+}";
+            var root = ParseMethodBlock(code);
+
+            metric.Measure(root).Should().Be(3);
+        }
+
+        [TestMethod]
+        public void Given_method_with_2_while_of_1_condition_when_metric_measure_should_be_3()
+        {
+            var code = @"
+public int WhileMethod(int i)
+{
+    while (i > 20)
+        i -= 10;
+    while (i > 10)
+        i -= 5;
+    return i;
+}";
+            var root = ParseMethodBlock(code);
+
+            metric.Measure(root).Should().Be(3);
+        }
+
+        [TestMethod]
+        public void Given_method_with_2_for_of_1_condition_when_metric_measure_should_be_3()
+        {
+            var code = @"
+public int ForMethod(int i)
+{
+    for (var j = 0; j < i; j++)
+    {
+        i += 10;
+    }
+    for (var e = 0; e < i+5; e++)
+    {
+        i -= 5;
+    }
+    return i;
+}";
+            var root = ParseMethodBlock(code);
+
+            metric.Measure(root).Should().Be(3);
+        }
+
+        [TestMethod]
+        public void Given_method_with_2_foreach_of_1_condition_when_metric_measure_should_be_3()
+        {
+            var code = @"
+public int ForeachMethod(int[] arr)
+{
+    var res = 0;
+    foreach (var v in arr)
+    {
+        res += v;
+    }
+    foreach (var v in arr)
+    {
+        res += v;
+    }
+    return res;
+}";
+            var root = ParseMethodBlock(code);
+
+            metric.Measure(root).Should().Be(3);
+        }
+
+        [TestMethod]
+        public void Given_method_with_switch_2_cases_of_1_condition_when_metric_measure_should_be_3()
+        {
+            var code = @"
+public int SwitchCaseMethod(int i)
+{
+    switch (i)
+    {
+        case 20:
+            i += 10;
+            break;
+        case 50:
+            i += 5;
+            break;
+    }
+    return i;
+}";
+            var root = ParseMethodBlock(code);
+
+            metric.Measure(root).Should().Be(3);
+        }
+
+        [TestMethod]
+        public void Given_method_with_2_switch_cases_of_1_condition_when_metric_measure_should_be_3()
+        {
+            var code = @"
+public int SwitchCaseMethod(int i)
+{
+    switch (i)
+    {
+        case 20:
+            i += 10;
+            break;
+    }
+
+    switch (i)
+    {
+        case 50:
+            i += 15;
+            break;
+    }
+    return i;
+}";
+            var root = ParseMethodBlock(code);
+
+            metric.Measure(root).Should().Be(3);
+        }
+
+        [TestMethod]
+        public void Given_method_with_2_for_continue_of_1_condition_when_metric_measure_should_be_3()
+        {
+            var code = @"
+public int ForContinueMethod(int i)
+{
+    for (var j = 0; j < i; j++)
+    {
+        i += 10;
+        continue;
+    }
+    for (var j = 0; j < i; j++)
+    {
+        i += 10;
+        continue;
+    }
+    return i;
+}";
+            var root = ParseMethodBlock(code);
+
+            metric.Measure(root).Should().Be(3);
+        }
+
+        [TestMethod]
+        public void Given_method_with_2_foreach_continue_of_1_condition_when_metric_measure_should_be_3()
+        {
+            var code = @"
+public int ForeachContinueMethod(int[] arr)
+{
+    var res = 0;
+    foreach (var v in arr)
+    {
+        res += v;
+        continue;
+    }
+
+    foreach (var v in arr)
+    {
+        res += v;
+        continue;
+    }
+    return res;
+}";
+            var root = ParseMethodBlock(code);
+
+            metric.Measure(root).Should().Be(3);
+        }
+
+        [TestMethod]
+        public void Given_method_with_2_goto_of_1_condition_when_metric_measure_should_be_1()
+        {
+            var code = @"
+public int GotoMethod(int i)
+{
+    goto teste;
+
+teste:
+    i = 10;
+    goto teste2;
+
+teste2:
+    return i;
+}";
+            var root = ParseMethodBlock(code);
+
+            metric.Measure(root).Should().Be(1);
+        }
+
+        [TestMethod]
+        public void Given_method_with_try_2_catch_of_1_condition_when_metric_measure_should_be_3()
+        {
+            var code = @"
+public int TryCatchMethod(int i, int j)
+{
+    try
+    {
+        i = i / j;
+    }
+    catch (ArithmeticException ex)
+    {
+        i = 0;
+    }
+    catch (Exception ex)
+    {
+        i = 0;
+    }
+
+    return i;
+}";
+            var root = ParseMethodBlock(code);
+
+            metric.Measure(root).Should().Be(3);
+        }
+
+        [TestMethod]
+        public void Given_method_with_2_ternary_operator_of_1_condition_when_metric_measure_should_be_3()
+        {
+            var code = @"
+public int TernaryOperatorMethod(int i)
+{
+    i = i > 20 ? 10 : 20;
+    i = i < 10 ? 0 : 20;
+    return i;
+}";
+            var root = ParseMethodBlock(code);
+
+            metric.Measure(root).Should().Be(3);
+        }
+
+        [TestMethod]
+        public void Given_method_with_2_null_coalescing_operator_of_1_condition_when_metric_measure_should_be_3()
+        {
+            var code = @"
+public int NullCoalescingOperatorMethod(object i)
+{
+    i = i ?? null;
+    i = i ?? 50;
+    return Convert.ToInt32(i);
+}";
+            var root = ParseMethodBlock(code);
+
+            metric.Measure(root).Should().Be(3);
+        }
 
         #endregion
 
